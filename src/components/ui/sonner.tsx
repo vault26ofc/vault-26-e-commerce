5710ccacd2c5225a4276636cbbb11f41f69a76d3
atchyuthkarri <atchyuthkarri46@gmail.com>
2026-08-24 11:09:38 +0530

    fix: resolve blank white notification toasts by fixing CSS text color contrast in Sonner

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -12,17 +12,17 @@ const Toaster = ({ ...props }: ToasterProps) => {
         classNames: {
           toaster: "!font-ui",
           toast:
-            "!rounded-none !border !border-black/10 !bg-white !text-black !shadow-[0_4px_40px_rgba(0,0,0,0.12)] !px-5 !py-4 !gap-3 !font-ui",
+            "group toast !rounded-none !border !shadow-[0_4px_40px_rgba(0,0,0,0.12)] !px-5 !py-4 !gap-3 !font-ui !bg-white !text-black !border-black/10",
           title:
-            "!text-[11px] !tracking-[0.18em] !uppercase !font-bold !text-black !font-ui",
+            "!text-[11px] !tracking-[0.18em] !uppercase !font-bold !font-ui text-inherit",
           description:
-            "!text-[10px] !tracking-[0.08em] !text-black/50 !font-ui !mt-0.5",
+            "!text-[10px] !tracking-[0.08em] !font-ui !mt-0.5 opacity-70 text-inherit",
           error:
-            "!bg-black !text-white !border-black [&_[data-title]]:!text-white [&_[data-description]]:!text-white/60",
+            "!bg-black !text-white !border-black [&_[data-title]]:!text-white [&_[data-description]]:!text-white/70",
           success:
-            "!bg-white !text-black !border-l-2 !border-l-black !border-t-0 !border-r-0 !border-b-0",
+            "!bg-white !text-black !border-l-4 !border-l-black !border-t-0 !border-r-0 !border-b-0 [&_[data-title]]:!text-black [&_[data-description]]:!text-black/60",
           warning:
-            "!bg-white !text-black !border-l-2 !border-l-amber-500 !border-t-0 !border-r-0 !border-b-0",
+            "!bg-white !text-black !border-l-4 !border-l-amber-500 !border-t-0 !border-r-0 !border-b-0 [&_[data-title]]:!text-black [&_[data-description]]:!text-black/60",
           actionButton:
             "!rounded-none !bg-black !text-white !text-[10px] !tracking-[0.2em] !uppercase !font-bold !font-ui",
           cancelButton:
